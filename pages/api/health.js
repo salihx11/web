f543aa0f16d5a8bf0582c86f8aadaa4faf7c6869
marco <115997266+salihx11@users.noreply.github.com>
2025-11-03 16:56:45 +0530

    Delete pages directory

--- pages/api/health.js
+++
@@ -1,7 +0,0 @@
-// Simple API route to fix Vercel deployment
-export default function handler(req, res) {
-  res.status(200).json({ 
-    status: 'OK', 
-    message: 'CoinSpark Admin API is running' 
-  })
-}
